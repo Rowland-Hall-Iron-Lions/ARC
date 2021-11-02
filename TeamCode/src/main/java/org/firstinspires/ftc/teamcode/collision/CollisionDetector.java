a5fc7fc089e2f49a29886d571c8aff96f28298b9
599274 <raphaelandreae@rowlandhall.org>
2021-11-02 17:29:00 -0600

    Fixed Formatting in Collision Dectector

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/collision/CollisionDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/collision/CollisionDetector.java
@@ -2,6 +2,19 @@ package org.firstinspires.ftc.teamcode.collision;
 
 public class CollisionDetector {
 
-    //We need a distance sensor
+    // This code will be initiated after the ducks have been read
+    // We need a distance sensor
+    // We will add motor stuff
+    // This will override other sensor data
 
+    int Distance;
+
+    Boolean detectIfNeeded() {
+        // Detect if we are within 12 inches
+        if (Distance <= 12) {
+            // We do need to change something; communicate this to the caller
+            return true;
+
+        }
+    }
 }
